4cc6dd65586f1fa71b75ea4f085cd1a7ad9a7328
HeLLxGodLike <ysonu4951@gmail.com>
2021-07-04 15:17:41 +0530

    Update __pyrosongs.py

--- DaisyXMusic/modules/__pyrosongs.py
+++ DaisyXMusic/modules/__pyrosongs.py
@@ -5,7 +5,7 @@ import aiohttp
 import youtube_dl
 
 from pyrogram import filters
-from SaitamaRobot import pgram as bot
+from DaisyXMusic import pgram as bot
 from youtube_search import YoutubeSearch
 
 
